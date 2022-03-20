a8291f71500371d18737f541a9480a113ee83a59
Muqoffin <ofinakli26@gmail.com>
2022-03-20 10:45:31 +0700

    update new

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,6 +10,18 @@
                 typeSpeed: 40,
                 loop: true
             });
+            var typed = new Typed('#typed-slide-2', {
+                stringsElement: '#typed-strings-slide-2',
+                backSpeed: 40,
+                typeSpeed: 40,
+                loop: true
+            });
+            var typed = new Typed('#typed-slide-3', {
+                stringsElement: '#typed-strings-slide-3',
+                backSpeed: 40,
+                typeSpeed: 40,
+                loop: true
+            });
         }
         // lib typed end 
 
